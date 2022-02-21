19d4ee3a9d3b544de33736baf38dc0d1e4300326
edimsi <85470476+edimsi@users.noreply.github.com>
2022-02-21 17:36:35 +0700

    first

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SERVER_URL: "https://5w1wgzbemm9f.bigmoralis.com:2053/server",
+    NEXT_PUBLIC_APP_ID: "7uQ481cNxNDgFLv3YqzzM76X7avmta4kivEpAi5t",
+    TOKEN_V1: "0xc98a8ec7a07f1b743e86896a52434c4c6a0dbc42",
+    TOKEN_V2: "0x34bcA9c45a41280174024799f477A3062fE279b9"
+  },
+};
